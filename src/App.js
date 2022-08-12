27feb6ad1d245db0549ac64c4059e2d5b1d705fc
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-12 12:35:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React from 'react';
+import styles from  './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className={styles.WholeContainer}>
+      <div className={styles.App}>
+        <header className={styles.header}>
+          <a href='#about'>
+            About
+          </a>        
+          <h1>
+            Baltej Randhawa
+          </h1>
+          <a href='#portfolio'>
+            portfolio
+          </a>
+        </header>
+        <section className={styles.section}>
+          <h1>Hi, I'm Baltej</h1>
+        </section>
+        
+      </div>
     </div>
   );
 }

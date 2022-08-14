3807cfa9cfe1bd00c21f22ec245d05f9fa3f7f6f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-13 23:27:51 -0700

    removed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import styles from  './App.css';
+import './App.css';
 import Odin from './images/odin.png'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
+
   return (
-    <div className={styles.WholeContainer}>
-      <div className={styles.App}>
-        <section id="about" className={styles.introSection}>
-          <Navbar /> 
+    <div className='WholeContainer'>
+      <div className='App'>
+        <Navbar /> 
+        <section id="about" className='introSection'>
           <h1>Hi, I'm Baltej</h1>
           <h3>
             My name is Baltej Randhawa, and I am a full stack web developer from Vancouver BC.
           </h3>
-          <img className={styles.odinImg} src={Odin} alt="The odin project"></img>
-        </section>        
-        <section id="portfolio" className={styles.introSection}>
-          <h1>Hi, I'm portfolio</h1>
-          <h1>I'm a full stack devloper </h1>
-          <h3>I learned my these skills through The Odin Project. The projects started out small
-          </h3>
-          <img className={styles.odinImg} src={Odin} alt="The odin project"></img>
-        </section>        
+          <img className='odinImg' src={Odin} alt="The odin project"></img>
+        </section>              
       </div>
     </div>
   );

16dcc61ffd0d32542c067c07109905c5dd567cec
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-13 17:05:45 -0700

    create navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () =>{
+
+  return(
+    <div className={styles.mainContainer}>
+      NAVBAR
+    </div>
+  );
+};
+
+export default Navbar;

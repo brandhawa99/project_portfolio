1483a1a1054ca144d3ef1d5cc38d08d8b6dc8e2a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-13 23:28:17 -0700

    initialized navmenu

---
+++ src/components/Navmenu/Navmenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Navmenu.module.css';
+
+const Navmenu = () =>{
+
+  return(
+    <div className={styles.mainContainer}>
+
+    </div>
+  );
+};
+
+export default Navmenu;

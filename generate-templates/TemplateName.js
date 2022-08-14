5ed18fd0163b357c2c13ff6d84f6b933155c1367
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-13 17:05:20 -0700

    added genreate-react-cli

---
+++ generate-templates/TemplateName.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './TemplateName.module.css';
+
+const TemplateName = () =>{
+
+  return(
+    <div className={styles.mainContainer}>
+
+    </div>
+  );
+};
+
+export default TemplateName;

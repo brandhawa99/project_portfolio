7ff36d0386993a6241e4ae0d3b84be4de29452cf
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-16 12:19:40 -0700

    button initalized

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = () =>{
+
+  return(
+    <div className={styles.mainContainer}>
+
+    </div>
+  );
+};
+
+export default Button;

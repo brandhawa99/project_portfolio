c16371c529e700f15e1c53d33a367261e609f313
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-16 12:18:48 -0700

    nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import { MenuIcon } from '@heroicons/react/solid';
 
 const Navbar = () =>{
 
   return(
     <div className={styles.mainContainer}>
-
+      <h1>Baltej Randhawa</h1>
+      <div className={styles.buttonContainer}>
+        <button className={styles.navButton}>About</button>
+        <button className={styles.navButton}>Portfolio</button>
+        <button className={styles.navButton}>Contact</button>
+      </div>
+      <div className={styles.menuIcon}>
+        <MenuIcon width={24} />
+      </div>
     </div>
   );
 };

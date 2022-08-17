2fa82a81de957d4bd381ca70ecda4d82739e6291
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-17 16:23:11 -0700

    portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './Portfolio.module.css';
 import HeaderText from '../HeaderText/HeaderText';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faJs, faHtml5, faCss3 } from '@fortawesome/free-brands-svg-icons';
+import { faNode, faGitAlt, faGithub, faNpm } from '@fortawesome/free-brands-svg-icons';
 
 const Portfolio = () =>{
 
@@ -14,10 +15,46 @@ const Portfolio = () =>{
         <h2 className={styles.smallHeader}>Skills</h2>
           <div>
             <h3>Front-End</h3>
-              <div>
-              <FontAwesomeIcon icon={faReact}/>
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon className={styles.tooltip} icon={faReact}/>
+                <FontAwesomeIcon className={styles.tooltip}icon={faJs}/>
+                <FontAwesomeIcon  className={styles.tooltip}icon={faHtml5}/>
+                <FontAwesomeIcon  className={styles.tooltip} icon={faCss3}/>
+                <div className={styles.iconText}>Material UI</div>
+                <div className={styles.iconText}>Tailwind</div>
+                <div className={styles.iconText}>Jest</div>
+                <div className={styles.iconText}>Webpack</div>
               </div>
           </div>
+          <div>
+            <h3>Back-End</h3>
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon className={styles.tooltip}  icon={faNode}/>
+                <div className={styles.iconText}>Express js</div>
+                <div className={styles.iconText}>MongoDB</div>
+                <div className={styles.iconText}>Mongoose</div>
+                <div className={styles.iconText}>Restful API's</div>
+                <div className={styles.iconText}>Heroku</div>
+                <div className={styles.iconText}>Firebase</div>
+              </div>
+          </div>
+          <div>
+            <h3>Other Tools</h3>
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon className={styles.tooltip} icon={faGitAlt}/>
+                <FontAwesomeIcon className={styles.tooltip} icon={faGithub}/>
+                <FontAwesomeIcon className={styles.tooltip} icon={faNpm}/>
+                <div className={styles.iconText}>Github Actions</div>
+              </div>
+          </div>
+
+      </div>
+
+      <div>
+        <h2>Projects</h2>
+        <div>
+            
+        </div>
 
       </div>
     </div>

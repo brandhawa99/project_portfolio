d483500f52b45637a166a0c922a653bb190b1d3f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-16 23:58:53 -0700

    added components to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import FirstSection from './components/FirstSection/FirstSection';
 import About from './components/About/About'
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
 
@@ -12,7 +13,10 @@ function App() {
         <Navbar />
         <div className='underNav'>
         <FirstSection />
-        <About />
+        <div className='mainContent'>
+          <About />
+          <Portfolio />
+        </div>
         </div>
       </div>
     </div>

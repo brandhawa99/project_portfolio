6df78a9f501e64f107b598ca68abe0905b721eb0
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-16 23:55:09 -0700

    initialize sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Sidebar.module.css';
+
+const Sidebar = () =>{
+
+  return(
+    <div className={styles.mainContainer}>
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;

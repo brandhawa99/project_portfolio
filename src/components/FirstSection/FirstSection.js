bed9c74fa7370adc77aef44046de20046652939c
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-10 11:40:21 -0700

    added gsap

--- src/components/FirstSection/FirstSection.js
+++ src/components/FirstSection/FirstSection.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import styles from './FirstSection.module.css';
-import { ArrowDownIcon } from '@heroicons/react/solid'
+import React, { useEffect, useRef } from "react";
+import styles from "./FirstSection.module.css";
+import { ArrowDownIcon } from "@heroicons/react/solid";
+import { gsap } from "gsap/dist/gsap";
+import { Power3 } from "gsap";
+const FirstSection = () => {
+  let textContainer = useRef(null);
+  let name = useRef(null);
+  let button = useRef(null);
+  useEffect(() => {
+    gsap.from(button, {
+      duration: 2,
+      opacity: 0,
+      delay: 0.4,
+      y: 50,
+      ease: Power3.easeIn,
+    });
+    gsap.from(name, {
+      duration: 1,
+      opacity: 0,
+      delay: 0.3,
+      y: -80,
+      ease: Power3.easeIn,
+    });
 
-const FirstSection = () =>{
+    gsap.from(textContainer, {
+      duration: 1,
+      opacity: 0,
+      delay: 0.2,
+      y: +50,
+      ease: Power3.easeIn,
+    });
+  }, []);
 
-  return(
+  return (
     <div className={styles.mainContainer}>
-      <div className={styles.textContainer}>
-      <div className={styles.centeredText}>
-        Hey, welcome to my portfolio. I'm &nbsp;
-        <span className={styles.text}>Baltej Randhawa</span> 
-        &nbsp;, and I am a fullstack developer currently looking for work!
+      <div ref={(el) => (textContainer = el)} className={styles.textContainer}>
+        <div className={styles.centeredText}>
+          Hey, welcome to my portfolio. I'm &nbsp;
+          <span ref={(el) => (name = el)} className={styles.text}>
+            Baltej Randhawa
+          </span>
+          &nbsp;, and I am a fullstack developer currently looking for work!
         </div>
       </div>
-      <a className={styles.checkOut} href='#about'>
-         <div className={styles.butText}>
-            Check me out
-          </div>
-          <ArrowDownIcon width={20}  className={styles.arrow} />
+      <a ref={(el) => (button = el)} className={styles.checkOut} href="#about">
+        <div className={styles.butText}>Check me out</div>
+        <ArrowDownIcon width={20} className={styles.arrow} />
       </a>
     </div>
   );

d2a8980c0376eec4d5e23492fd3659f43d78fbde
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-10 11:40:34 -0700

    added ref

--- src/components/HeaderText/HeaderText.js
+++ src/components/HeaderText/HeaderText.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import styles from './HeaderText.module.css';
+import React from "react";
+import styles from "./HeaderText.module.css";
 
-const HeaderText = (props) =>{
-
-  return(
-    <h1 className={styles.mainContainer}>
-        {props.text}
+const HeaderText = (props) => {
+  return (
+    <h1 ref={props.ref} className={styles.mainContainer}>
+      {props.text}
     </h1>
   );
 };

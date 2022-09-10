376c176f1cdc0edf9ec3cd4024840fd5d73ddb43
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-09 23:48:07 -0700

    created data  for projects

---
+++ src/projects-data.js
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    id: 1,
+    title: "Shopping Cart App",
+    blog: "This is a thing that i made wehere this and that and then. We were like this then that . then i wasa liek this that okay hahaah",
+    tools: "Express js and then this then that and that okkay hahaha",
+    imgLink:
+      "https://user-images.githubusercontent.com/35308786/185670287-ad051d88-aad5-46bf-91e3-f0b106328ed3.png",
+    github: "https://github.com/brandhawa99/shoppingcart_odin",
+    link: "https://shopping-cart-app-brandhawa.netlify.app/shop",
+  },
+  {
+    id: 2,
+    title: "Blog Api",
+    blog: "This is a thing that i made wehere this and that and then. We were like this then that . then i wasa liek this that okay hahaah",
+    tools: "Express js and then this then that and that okkay hahaha",
+    imgLink:
+      "https://images.unsplash.com/photo-1662556224711-c8a08f614978?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80ahaah",
+    github: "www.github.com",
+    link: "hahaah",
+  },
+  {
+    id: 3,
+    title: "Blog Api",
+    blog: "This is a thing that i made wehere this and that and then. We were like this then that . then i wasa liek this that okay hahaah",
+    tools: "Express js and then this then that and that okkay hahaha",
+    imgLink:
+      "https://images.unsplash.com/photo-1662556224711-c8a08f614978?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80ahaah",
+    github: "www.github.com",
+    link: "hahaah",
+  },
+];
+
+export default projects;

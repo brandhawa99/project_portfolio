3c793ab60f3122619fd174fa5a7045f3d9024c7d
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-09 23:46:29 -0700

    update nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import styles from './Navbar.module.css';
-import { MenuIcon } from '@heroicons/react/solid';
+import React from "react";
+import styles from "./Navbar.module.css";
+import { MenuIcon } from "@heroicons/react/solid";
 
-const Navbar = () =>{
-
-  return(
+const Navbar = () => {
+  return (
     <div className={styles.mainContainer}>
-      <h1>Baltej Randhawa</h1>
+      <h1 className={styles.text}>
+        <a className={styles.none} href="#">
+          Baltej Randhawa
+        </a>
+      </h1>
       <div className={styles.buttonContainer}>
-        <a href="#about" className={styles.navButton}>About</a>
-        <a href="#about" className={styles.navButton}>Portfolio</a>
-        <a href="#about" className={styles.navButton}>Contact</a>
+        <a href="#about" className={styles.navButton}>
+          About
+        </a>
+        <a href="#portfolio" className={styles.navButton}>
+          Portfolio
+        </a>
+        <a href="#contact" className={styles.navButton}>
+          Contact
+        </a>
       </div>
       <div className={styles.menuIcon}>
         <MenuIcon width={24} />

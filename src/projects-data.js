5ecf76b739e1c0c593d5bf5dde81d1bba27a3c7d
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-11 01:00:54 -0700

    spell checked data

--- src/projects-data.js
+++ src/projects-data.js
@@ -2,7 +2,7 @@ const projects = [
   {
     id: 1,
     title: "Shopping Cart App",
-    blog: "This app fetch data from a fake store REST API. I display all the products on the store page where you can see the price and name. Once you click on a product you can get a detailed view of the item with a description. You can add products at a cart which updates its count automatically. Once you click on the cart you can see everything that was added with a count and price.",
+    blog: "This app fetches data from a fake store REST API. I display all the products on the store page where you can see the price and name. Once you click on a product you can get a detailed view of the item with a description. You can add products to a cart which updates its count automatically. Once you click on the cart you can see everything that was added with a count and price.",
     tools: "Built with reactjs, javascript, and css  ",
     imgLink:
       "https://user-images.githubusercontent.com/35308786/185670287-ad051d88-aad5-46bf-91e3-f0b106328ed3.png",
@@ -12,7 +12,7 @@ const projects = [
   {
     id: 2,
     title: "Blog API",
-    blog: "This RESTful API has two major routes, a client route and an content management route. The client route displays blog posts create by authors and gives viewers the ability to comment on the blog posts. The CMS route lets authors sign-in and manage posts(make them public or private), create and update posts. Also authors can manage the comments under their posts.",
+    blog: "This RESTful API has two major routes, a client route, and a content management route. The client route displays blog posts created by authors and gives viewers the ability to comment on the blog posts. The CMS route lets authors sign in and manage posts(make them public or private) and create and update posts. Also, authors can manage the comments under their posts.",
     tools:
       "Built with nodejs, expressjs, mongodb  and passportjs(JWT Strategy). Tested with jest, and supertest ",
     imgLink:
@@ -23,7 +23,7 @@ const projects = [
   {
     id: 3,
     title: "Blog Client",
-    blog: "This react app connects to the 'Blog API' and gets all of the public posts create by authors. Once you get at an individual post you can post a comment under it.",
+    blog: "This react app connects to the 'Blog API' and gets all of the public posts created by the authors. Once you get at an individual post you can post a comment under it.",
     tools: "Built with reactjs, javascript, css, ",
     imgLink:
       "https://user-images.githubusercontent.com/35308786/188829854-f26ca268-7508-4d9e-9b6c-6913430ac0f2.png",
@@ -32,7 +32,7 @@ const projects = [
   {
     id: 4,
     title: "Blog CMS",
-    blog: "This react app connects to the 'Blog API' and gets users to login and manage their content. It stores JWT during authentication and passes it around in the authorzation header when making requests. Login with username: bawa, password: password123",
+    blog: "This react app connects to the 'Blog API' and gets users to log in and manage their content. It stores JWT during authentication and passes it around in the authorization header when making requests. Login with username: bawa, password: password123",
     tools: "Built with reactjs, javascript, css, ",
     imgLink:
       "https://user-images.githubusercontent.com/35308786/188833160-235e8ab1-5df6-41c8-8654-b9b048fe768b.png",

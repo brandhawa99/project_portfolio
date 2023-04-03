694210da5cea237d012d25257736017506fc1518
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-04-03 15:04:46 -0700

    changed sizes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Projects.module.css';
 import Project from './Project/Project'
 import projects from '../../projects-data'
+import { v4 } from 'uuid'
 const Projects = () => {
 
   return (
@@ -9,7 +10,7 @@ const Projects = () => {
       <h1>Projects</h1>
       <div className={styles.proj}>
         {projects.map(proj => {
-          return <Project title={proj.title} blog={proj.blog} tools={proj.tools} link={proj.link} github={proj.github} imgLink={proj.imgLink} />
+          return <Project key={v4()} title={proj.title} blog={proj.blog} tools={proj.tools} link={proj.link} github={proj.github} imgLink={proj.imgLink} />
         })}
       </div>
 

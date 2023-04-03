46221603699dd8ff63e455e7fac8a5ad10722b08
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-04-02 23:26:35 -0700

    updated menu

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Menu.module.css';
+
+const Menu = () => {
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbar__list}>
+        <li className={styles.navbar__item}><a href="#">BR</a></li>
+        <li className={styles.navbar__item}><a href="#about">About</a></li>
+        <li className={styles.navbar__item}><a href="#tools">Tools</a></li>
+        <li className={styles.navbar__item}><a href="#projects">Projects</a></li>
+        <li className={styles.navbar__item}><a href="#contacts">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;

7fbe5ef921bdce3218fa5e2e56526da4142d864a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-04-03 11:30:47 -0700

    typo

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
       <h1>Hello,</h1>
       <h3>My name is Baltej Randhawa.</h3>
       <p>
-        I am software developer from Vancouver, BC. I have almost 2 years of experience building full-stack applicatoins. I am currently looking for work, so thank you for checkout out my portfolio.
+        I am software developer from Vancouver, BC. I have almost 2 years of experience building full-stack applications. I am currently looking for work, so thank you for checkout out my portfolio.
       </p>
       <a className={styles.button}>Learn About Me</a>
     </div>
